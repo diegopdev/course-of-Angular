5101ceaa0c24180f1e5e3168b211977503b3bc1d
Diego Pereira de Assis <playskil@gmail.com>
2019-11-20 22:20:11 -0300

    :rocket: first commit

---
+++ aula04 - typescript/main.js
@@ -0,0 +1,8 @@
+var minhaVar = 'Minha variavel';
+var num = 5;
+var PI = 3.4;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ aula04 - typescript/main.ts
@@ -0,0 +1,14 @@
+var minhaVar = 'Minha variavel'
+
+let num = 5
+const PI = 3.4
+
+var numeros = [1, 2, 3]
+numeros.map(function(valor) {
+    return valor * 2
+})
+
+numeros.map(valor => valor * 2 )
+
+
+    
